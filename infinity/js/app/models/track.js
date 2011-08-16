99a0d7ce0c3bf7ad589bd51284541ac297da80a5
Jakub Kulak <j.kulak@gadu-gadu.pl>
2011-08-16 18:02:12 +0200

    using backbone model collection to display data

--- infinity/js/app/models/track.js
+++ infinity/js/app/models/track.js
@@ -1,63 +1,24 @@
 var Track = Backbone.Model.extend({
-	initialize: function () {
-		console.log('initializing Track');	
+
+	defaults: {
+		// title: 'default_title'
 	},
+
+	initialize: function (data) {
+
+	},
+
 	save: function () {
-		tasks.add(this);
+
+		tracks.add(this);
+	},
+
+	toRender: function () {
+
+		return {
+			title: this.get('title'),
+			imageUrl: this.get('artwork_url') || this.get('user').avatar_url,
+			artist: this.get('user').username
+		};
 	}
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// SPINE
-// var TrackStore = [];
-
-// var Track = Spine.Model.setup("Track", ["artist", "title", "id"]);
-
-// Track.extend({
-// 	importAll: function (data) {
-
-// 		$.each(data, function(value, key){
-// 			console.log(key.user.username + ' - ' + key.title);
-// 			var track = Track.init({title: key.title, artist: key.user, yo: 'yoooo'});
-// 			console.log('track', track);
-// 			track.save();
-// 		});
-// 	}
-// });
-
-// Track.include({
-//   getAll: function(){
-//     return(this.artist.username + " - " + this.title);
-//   } 
-// });
--- infinity/js/infinity.js
+++ infinity/js/infinity.js
@@ -1,67 +1,36 @@
 var App = Backbone.Router.extend({
+    // things to be initialized on app start
     initialize: function () {
-        infinity.apihandler.init('#body');
-        infinity.timeline.init('#main-list');
-        infinity.statbox.init('#statbox');
 
         infinity.ui.init();
-
-        infinity.apihandler.getTracks();
+        appView.render();
     },
+
     routes: {
+        '!/:action':         'main',
         '!/track/id-:id':    'viewTrack',
-        '':                  'main'
-        
-        // "!/help":                 "help",    // #help
-        // "!/search/:query":        "search",  // #search/kiwis
-        // "!/search/:query/p:page": "search"   // #search/kiwis/p7
+        '*route':            'notFound'
+    },
+
+    notFound: function (route) {
+        console.log('router->' + route + ' (404, route not defined)');
     },
 
     main: function (action) {
         console.log('router->main (' + action + ')');
-        tracks.fetch(); 
     },
 
     viewTrack: function (id) {
-        console.log('router->main');
-        console.log('viewing track: ' . id);
+        console.log('router->track(' + id + ')');
+        // this.navigate("!/track/id-" + id);
     }
 });
 
 
 $(function(){
-    // bind event bus events
-    $.subscribe('apihandler:gettracks:success', function(data) {
-
-        infinity.timeline.append(data);
-        // TODO: use Model, and not DOM :/
-        infinity.statbox.updateTracksCount($('#main-list ul').children().size());
-    });
-
-    // bind DOM events
-    $(window).bind('resize', infinity.ui.init);
-    
-    $(window).bind('scroll', function() {
-
-        if($(window).scrollTop() + $(window).height() > $(document).height() - (0.125*$(document).height())) {
-            infinity.apihandler.getTracks();
-        }
-    });
-    
-    $('#action-reaload-tracks').bind('click', function() {
-        infinity.timeline.clearList();
-        infinity.apihandler.getTracks();
-    });
-
-    // img error handling
-    $('#main-list ul li img').bind('error', function () {
-        $(this).attr('src', 'images/imgerror.png');
-    });
-
     // Run application
     var app = new App();
-
+    
     // Start Backbone history a neccesary step for bookmarkable URL's
-    Backbone.history.start();
-
+    Backbone.history.start();   
 });
