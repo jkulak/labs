ba279f4923a70dc68aed17520c47e0606cdcb8e3
Jakub Kułak <jakub.kulak@gmail.com>
2016-12-15 01:00:30 -0600

    Updated version of beat machine

--- src/beat-machine/js/main.js
+++ src/beat-machine/js/main.js
@@ -1,1 +1,28 @@
+(function () {
+    'use strict';
 
+    function playSound(e) {
+        const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+        const key = document.querySelector(`li[data-key="${e.keyCode}"]`);
+        if (!audio) {
+            return;
+        } // stop the function from running altogether
+        audio.currentTime = 0; // rewind to the start
+        audio.play();
+        key.classList.add('playing');
+    }
+
+    function removeTransition(e) {
+        if (e.propertyName !== 'transform') {
+            return;
+        } // skip it if it's not a transform
+        this.classList.remove('playing');
+    }
+    const keys = document.querySelectorAll('li');
+    keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+    window.addEventListener('keydown', playSound);
+}());
+
+
+
+// https://www.youtube.com/watch?v=fw-_mvgoVcE
