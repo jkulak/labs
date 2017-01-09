3ae52e701ae873a1e461a8e040aa343e58bf0ea9
Jakub Kułak <jakub.kulak@gmail.com>
2017-01-09 11:41:32 +0100

    Add 3rd js30 exercise

--- src/js30/03-update-css-vars-with-js/main.js
+++ src/js30/03-update-css-vars-with-js/main.js
@@ -1,6 +1,23 @@
 (function(){
 'use strict';
 
+// const inputs = document.getElementsByTagName('input');
+const inputs = document.querySelectorAll('#controlpanel input');
 
+function handleUpdate() {
+    // this is the element on which the event was trigerred
+    if ('checkbox' === this.type) {
+        // Handle checkboxes
+        const bw = (this.checked) ? 1 : 0 ;
+        document.documentElement.style.setProperty(`--${this.name}`, bw);
+    } else {
+        const suffix = this.dataset.sizing || '';
+        // Handle other inputs
+        document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+    }
+}
+
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
 
 })();
